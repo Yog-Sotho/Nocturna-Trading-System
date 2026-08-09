7f9a3ba6d53af6e99656805ccf83d3f7aca66c58
Yog-Sotho <monnocellula@gmail.com>
2026-08-09 03:08:19 +0200

    chore(core): export EventBus and OrderStateMachine

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,2 +1,13 @@
 # Core trading engine module
 
+from .event_bus import Event, EventBus, default_bus
+from .order_state_machine import OrderState, OrderStateMachine, OrderStateRecord
+
+__all__ = [
+    "Event",
+    "EventBus",
+    "default_bus",
+    "OrderState",
+    "OrderStateMachine",
+    "OrderStateRecord",
+]
